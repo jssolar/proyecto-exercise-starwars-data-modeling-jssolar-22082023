d54803b8a24f714e3018becf8bbf190e1807e6ba
JuanSolar <juansolarsolar@yahoo.com>
2023-08-23 08:05:30 +0000

    terminado

--- src/models.py
+++ src/models.py
@@ -5,28 +5,64 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    firt_name = Column(String(50), unique=True, nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    planet = relationship('Planet')
+    Favorite = relationship('favorite')
+    
+    
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    name = Column(String(50))
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    location = Column(String(50))
+    moons = Column(String(50))
+    suns = Column(String(50))
+    population = Column(String(50))
+    galaxi = Column(String(50))
+  
+
+
+
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    appearances = Column(String(100))
+    Locations = Column(String(100), ForeignKey('planet.id'))
+    Gender = Column(String(25))
+    Dimensions = Column(String)
+    Species = Column(String(50))
+    Weapons = Column(String(50))
+    Vehicles = Column(String(50))
+    Character = relationship('character')
+    
+    
+    
+
+
+
+
+
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+
+# Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
