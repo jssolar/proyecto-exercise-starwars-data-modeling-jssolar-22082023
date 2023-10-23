7af97924ef88729b0d332a6b8c0394ed04c8262a
JuanSolar <juansolarsolar@yahoo.com>
2023-10-23 20:43:00 +0000

    tablas corregidas

--- src/models.py
+++ src/models.py
@@ -8,45 +8,40 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    firt_name = Column(String(50), unique=True, nullable=False)
+    firt_name = Column(String(50),  nullable=False)
     last_name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
     email = Column(String(50), unique=True, nullable=False)
-    password = Column(String(100), nullable=False)
-    planet = relationship('Planet')
-    Favorite = relationship('favorite')
-    
-class Favorite(Base):
-    __tablename__ = 'favorite'
+    password = Column(String(250), nullable=False)
+
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    name = Column(String(50))
+    name = Column(String(250), nullable=False)
+
+
 
 class Planet(Base):
     __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
     location = Column(String(50))
     moons = Column(String(50))
     suns = Column(String(50))
     population = Column(String(50))
     galaxi = Column(String(50))
-  
-class Character(Base):
-    __tablename__ = 'character'
+    
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
     id = Column(Integer, primary_key=True)
-    appearances = Column(String(100))
-    Locations = Column(String(100), ForeignKey('planet.id'))
-    Gender = Column(String(25))
-    Dimensions = Column(String)
-    Species = Column(String(50))
-    Weapons = Column(String(50))
-    Vehicles = Column(String(50))
-    Character = relationship('character')
-    
-    
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user_character = Column(String, ForeignKey('character.id'))
+    user_planet = Column(String, ForeignKey('planet.id'))
+    user = relationship(User)
     
 
     def to_dict(self):
@@ -55,3 +50,9 @@ class Character(Base):
 
 # Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
+
+
+
+
+
